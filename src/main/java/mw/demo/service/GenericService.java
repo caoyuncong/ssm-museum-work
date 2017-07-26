86b970c3d3f5ff292b04f38787711ac105577c84
Administrator <2780837020@qq.com>
2017-07-26 15:29:40 +0800

    update

--- src/main/java/mw/demo/service/GenericService.java
+++ src/main/java/mw/demo/service/GenericService.java
@@ -4,6 +4,7 @@ import mw.demo.util.Pagination;
 import mw.demo.util.Pagination;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by mingfei.net@gmail.com
@@ -22,9 +23,13 @@ public interface GenericService<T extends Serializable, ID extends Number> {
 
     void modify(String statement, Object parameter);
 
-    T query(String statement, Object parameter);
+    T queryOne(String statement, Object parameter);
 
     T queryById(ID id);
+
+    List<T> queryList(String statement, Object parameter);
+
+    List<T> queryAll();
 
     Pagination<T> query(String statement, Object parameter, int currentPage);
 
